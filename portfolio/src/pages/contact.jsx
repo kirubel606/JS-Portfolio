68398b9d8fdade0e38ac6799e370320b40285338
kirubel606 <kiruabatu@gmail.com>
2023-11-25 21:57:47 +0300

    ..

--- portfolio/src/pages/contact.jsx
+++ portfolio/src/pages/contact.jsx
@@ -1,45 +1,59 @@
 import React from 'react';
 import Navbare from '../components/navbar';
 import Amongus from '../assets/images/Amongus.png';
-import 'react-icons';
+import "../assets/contact.css";
+import { AiFillInstagram, AiFillPhone, AiFillLinkedin, AiFillFacebook, AiFillGithub } from "react-icons/ai";
 
 const Contact = () => {
   return (
-    <div className='bg-black' style={{height:"100vh"}}>
+    <div className='bg-black'>
       <Navbare />
-      <div className="container">
+      <div className="container containero">
         <div className="row">
-          <div className="col-md-3">
-            <img src={Amongus} className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" style={{ borderRadius: "600px" }} />
+          <div className="text-center col-md-3">
+            <img src={Amongus} className="mt-5 img-fluid" alt="" style={{ borderRadius: "600px" }} />
+            
+            <div className=" align-items-center mt-5">
+                <a href="https://www.instagram.com/kirubel_fekadu" target="_blank" rel="noopener noreferrer" className="  text-white text-decoration-none"  style={{paddingLeft:"3%"}}>
+                  <AiFillInstagram className="me-2 icone" />
+                </a>
+                <a href="https://www.linkedin.com/in/kirubel606" target="_blank" rel="noopener noreferrer" className=" text-white  text-decoration-none"  style={{paddingLeft:"3%"}}>
+                  <AiFillLinkedin className="me-2 icone" />
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className=" text-white   text-decoration-none" style={{paddingLeft:"3%"}}>
+                  <AiFillFacebook className="me-2 icone"/>
+                </a>
+              </div>
           </div>
 
-          <div className="col-md-3">
-          </div>
+          <div className="col-md-3"></div>
 
-          <div className="col-md-6">
-            <h1 className='text-white mt-5 mb-5'>I am located at..</h1>
-            <div className="d-flex align-items-center">
-              <i className="bi bi-geo-alt-fill text-primary me-2"></i>
-              <p className="text-white">Ayat/Lemi kura</p>
-              <p className="text-white">Phone:</p>
-              <p className="text-white">Phone:</p>
-              <hr style={{backgroundColor:"white"}}></hr >
+          <div className="col-md-6 text-center">
+            <h1 className='text-white mt-5'>Contact</h1>
+            <h4 className='mb-5' style={{ color: "rgba(223, 223, 223, 0.8)" }}>How can we work together?</h4>
+            <div className="d-flex flex-column">
+              <div className="d-flex align-items-center mb-2">
+                <i className="bi bi-geo-alt-fill text-white me-2"></i>
+                <p className="text-white mb-0">Ayat/Lemi kura</p>
+              </div>
+              <div className="d-flex align-items-center mb-2">
+                <a href="tel:+251926765396" className="text-white d-flex align-items-center text-decoration-none">
+                  <AiFillPhone className="me-2" />
+                  <p className="text-white mb-0">+251926765396</p>
+                </a>
+              </div>
+              
+              <hr style={{ backgroundColor: "white" }} />
             </div>
             <div className="mb-3">
-              <label htmlFor="" className="form-label">Email</label>
-              <input type="email" className="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com" />
-              
-              <label htmlFor="" className="form-label"></label>
-              <textarea className="form-control" name="" id="" rows="3"></textarea>
-              <button type="submit" className="btn mt-2 btn-primary">Submit</button>
+            
+              <input type="text" name="name" id="name" className="mb-2 form-control" placeholder="Name" aria-describedby="suffixId"/>
+              <input type="phone" name="phonee" id="phone" className="mb-2 form-control" placeholder="Phone" aria-describedby="suffixId"/>
+              <input type="email" className="mb-2 form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com" />
+              <textarea className="form-control" placeholder="Write something" name="" id="" rows="3"></textarea>
+              <button type="submit" className="btn mt-2 btn-primary">Send</button>
             </div>
           </div>
-
-          <div className='text-white mb-5 col-md-3'>
-            <h1>new line</h1>
-          </div>
-          
-           
         </div>
       </div>
     </div>
